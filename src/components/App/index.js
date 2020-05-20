5f584c589cb20d28a75f0f9fe06fad880f190a8b
MaSkA6293 <MaSkA6293@yandex.ru>
2020-05-20 14:08:30 +0300

    work completed

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,144 +1,149 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import './index.scss';
+import "./index.scss";
 
-import ContentPanel from '../ContentPanel';
-import LeftPanel from '../LeftPanel'
+import ContentPanel from "../ContentPanel";
+import LeftPanel from "../LeftPanel";
 
 import { Route, useHistory } from "react-router-dom";
-import WaterButton from '../WaterButton';
-import CSSGrid from '../CSSGrid';
-import RadioButton from '../RadioButton';
-import FotoMountain from '../FotoMountain';
-import SearchButton from '../SearchButton';
-import AnimationButton from '../AnimationButton';
-import RotateCard from '../RotateCard';
+import WaterButton from "../WaterButton";
+import CSSGrid from "../CSSGrid";
+import RadioButton from "../RadioButton";
+import FotoMountain from "../FotoMountain";
+import SearchButton from "../SearchButton";
+import AnimationButton from "../AnimationButton";
+import RotateCard from "../RotateCard";
+import NeonButton from "../NeonButton";
 const base = [
-    {
-        id: 0,
-        title: 'Water button'
-    },
-    {
-        id: 1,
-        title: 'CSS Grid'
-    },
-    {
-        id: 2,
-        title: 'Radio button'
-    },
-    {
-        id: 3,
-        title: 'Foto mountain'
-    },
-    {
-        id: 4,
-        title: 'Search button'
-    },
-    {
-        id: 5,
-        title: 'Animation button'
-    },
-    {
-        id: 6,
-        title: 'Rotate card'
-    },
-]
+  {
+    id: 0,
+    title: "Water button",
+  },
+  {
+    id: 1,
+    title: "CSS Grid",
+  },
+  {
+    id: 2,
+    title: "Radio button",
+  },
+  {
+    id: 3,
+    title: "Foto mountain",
+  },
+  {
+    id: 4,
+    title: "Search button",
+  },
+  {
+    id: 5,
+    title: "Animation button",
+  },
+  {
+    id: 6,
+    title: "Rotate card",
+  },
+  {
+    id: 7,
+    title: "Neon button",
+  },
+];
 
 const App = () => {
-    const [activTab, setactivTab] = useState(0);
-    let history = useHistory();
+  const [activTab, setactivTab] = useState(0);
+  let history = useHistory();
 
+  useEffect(() => {
+    const id = history.location.pathname.split("list/")[1];
+    if (id) {
+      let elem = base.find((item) => item.id === Number(id));
+      if (!elem) {
+        history.push("/");
+        setactivTab(0);
+      } else {
+        setactivTab(elem.id);
+      }
+    }
+  }, [history.location.pathname]);
 
-    useEffect(() => {
-        const id = history.location.pathname.split('list/')[1];
-        if (id) {
-            let elem = base.find(item => item.id === Number(id))
-            if (!elem) {
-                history.push('/');
-                setactivTab(0);
-            }
-            else {
-                setactivTab(elem.id)
-            }
-        }
-    }, [history.location.pathname])
+  return (
+    <div className="App">
+      <Route exact path="/">
+        <div className="App__left-panel">
+          <LeftPanel list={base} />
+        </div>
+        <div className="App__content-panel">
+          <ContentPanel list={base[activTab]} component={<WaterButton />} />
+        </div>
+      </Route>
 
+      <Route path="/list/0">
+        <div className="App__left-panel">
+          <LeftPanel list={base} />
+        </div>
+        <div className="App__content-panel">
+          <ContentPanel list={base[activTab]} component={<WaterButton />} />
+        </div>
+      </Route>
 
-    return (
-        <div className='App'>
-            <Route exact path='/'>
-                <div className='App__left-panel'>
-                    <LeftPanel list={base} />
-                </div>
-                <div className='App__content-panel'>
-                    <ContentPanel list={base[activTab]} component={<WaterButton />} />
-                </div>
-            </Route>
+      <Route path="/list/1">
+        <div className="App__left-panel">
+          <LeftPanel list={base} />
+        </div>
+        <div className="App__content-panel">
+          <ContentPanel list={base[activTab]} component={<CSSGrid />} />
+        </div>
+      </Route>
 
-            <Route path='/list/0'>
-                <div className='App__left-panel'>
-                    <LeftPanel list={base} />
-                </div>
-                <div className='App__content-panel'>
-                    <ContentPanel list={base[activTab]} component={<WaterButton />} />
-                </div>
-            </Route>
-
-            <Route path='/list/1'>
-                <div className='App__left-panel'>
-                    <LeftPanel list={base} />
-                </div>
-                <div className='App__content-panel'>
-                    <ContentPanel list={base[activTab]} component={<CSSGrid />} />
-                </div>
-            </Route>
-
-            <Route path='/list/2'>
-                <div className='App__left-panel'>
-                    <LeftPanel list={base} />
-                </div>
-                <div className='App__content-panel'>
-                    <ContentPanel list={base[activTab]} component={<RadioButton />} />
-                </div>
-            </Route>
-            <Route path='/list/3'>
-                <div className='App__left-panel'>
-                    <LeftPanel list={base} />
-                </div>
-                <div className='App__content-panel'>
-                    <ContentPanel list={base[activTab]} component={<FotoMountain />} />
-                </div>
-            </Route>
-            <Route path='/list/4'>
-                <div className='App__left-panel'>
-                    <LeftPanel list={base} />
-                </div>
-                <div className='App__content-panel'>
-                    <ContentPanel list={base[activTab]} component={<SearchButton />} />
-                </div>
-            </Route>
-            <Route path='/list/5'>
-                <div className='App__left-panel'>
-                    <LeftPanel list={base} />
-                </div>
-                <div className='App__content-panel'>
-                    <ContentPanel list={base[activTab]} component={<AnimationButton />} />
-                </div>
-            </Route>
-            <Route path='/list/6'>
-                <div className='App__left-panel'>
-                    <LeftPanel list={base} />
-                </div>
-                <div className='App__content-panel'>
-                    <ContentPanel list={base[activTab]} component={<RotateCard />} />
-                </div>
-            </Route>
-
-
-
-        </div >
-    )
-
-
-}
-export default App
+      <Route path="/list/2">
+        <div className="App__left-panel">
+          <LeftPanel list={base} />
+        </div>
+        <div className="App__content-panel">
+          <ContentPanel list={base[activTab]} component={<RadioButton />} />
+        </div>
+      </Route>
+      <Route path="/list/3">
+        <div className="App__left-panel">
+          <LeftPanel list={base} />
+        </div>
+        <div className="App__content-panel">
+          <ContentPanel list={base[activTab]} component={<FotoMountain />} />
+        </div>
+      </Route>
+      <Route path="/list/4">
+        <div className="App__left-panel">
+          <LeftPanel list={base} />
+        </div>
+        <div className="App__content-panel">
+          <ContentPanel list={base[activTab]} component={<SearchButton />} />
+        </div>
+      </Route>
+      <Route path="/list/5">
+        <div className="App__left-panel">
+          <LeftPanel list={base} />
+        </div>
+        <div className="App__content-panel">
+          <ContentPanel list={base[activTab]} component={<AnimationButton />} />
+        </div>
+      </Route>
+      <Route path="/list/6">
+        <div className="App__left-panel">
+          <LeftPanel list={base} />
+        </div>
+        <div className="App__content-panel">
+          <ContentPanel list={base[activTab]} component={<RotateCard />} />
+        </div>
+      </Route>
+      <Route path="/list/7">
+        <div className="App__left-panel">
+          <LeftPanel list={base} />
+        </div>
+        <div className="App__content-panel">
+          <ContentPanel list={base[activTab]} component={<NeonButton />} />
+        </div>
+      </Route>
+    </div>
+  );
+};
+export default App;
---
+++ src/components/NeonButton/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./index.scss";
+
+const NeonButton = () => {
+  return (
+    <div className="neon-button-contaner">
+      <button>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        neon button
+      </button>
+    </div>
+  );
+};
+export default NeonButton;
